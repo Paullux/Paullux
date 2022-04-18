f284f07a2cbf965ac9d43d693e2770632352e778
Paul WOISARD <paulwoisard@gmail.com>
2022-04-18 18:17:40 +0200

    retrait proxy

--- services/puppeteer.service.js
+++ services/puppeteer.service.js
@@ -14,7 +14,6 @@ class PuppeteerService {
         '--ignore-certifcate-errors',
         '--ignore-certifcate-errors-spki-list',
         '--incognito',
-        '--proxy-server=http=194.67.37.90:3128',
         // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
       ],
       // headless: false,
